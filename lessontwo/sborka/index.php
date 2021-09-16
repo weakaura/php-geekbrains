332ce31f310a36dd8c33ef22473522c0ad6d583f
weakaura <79326606+weakaura@users.noreply.github.com>
2021-09-17 01:22:17 +0300

    homework 2
    homework 2

---
+++ lessontwo/sborka/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$about = renderTemplate('about', 777333);
+$menu = renderTemplate('menu', "мы хорошие");
+
+
+echo renderTemplate('layout', $menu.$about);
+
+function renderTemplate($page, $content = '') {
+    ob_start();
+    include $page . ".php";
+    return ob_get_clean();
+}
