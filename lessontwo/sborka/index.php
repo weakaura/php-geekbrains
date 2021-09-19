8231db368e9b652d7cb2936b19b21cecec265f7d
weakaura <79326606+weakaura@users.noreply.github.com>
2021-09-19 14:48:36 +0300

    1
    1

--- lessontwo/sborka/index.php
+++ lessontwo/sborka/index.php
@@ -4,9 +4,9 @@ $about = renderTemplate('about', 777333);
 $menu = renderTemplate('menu', "мы хорошие");
 
 
-echo renderTemplate('layout', $menu.$about);
+echo renderTemplate('layout', $menu, $about);
 
-function renderTemplate($page, $content = '') {
+function renderTemplate($page, $content = '', $about = '') {
     ob_start();
     include $page . ".php";
     return ob_get_clean();
